4e1697cab4de93d63cd49089824924e13021c441
Brendan Desmond <brendes@gmail.com>
2024-04-24 21:38:48 -0400

    simplify colors

--- src/themes.py
+++ src/themes.py
@@ -1,174 +1,135 @@
 #!/usr/bin/env python3
 
-light = {
-    "theme_type": "light",
-    "bg_match": "#e8d0b8",
-    "fg_bold": "#000000",
-    "red_1": "#bb5458",
-    "red_2": "#e8a0a8",
-    "orange_1": "#d08968",
-    "orange_2": "#f0ccb0",
-    "yellow_1": "#a88870",
-    "yellow_2": "#e8d0b8",
-    "green_1": "#889f78",
-    "green_2": "#afbf88",
-    "blue_1": "#7080a0",
-    "blue_2": "#b8c8d7",
-    "magenta_1": "#a07488",
-    "magenta_2": "#d8b8c8",
-    "cyan_1": "#6f9b98",
-    "cyan_2": "#b0d8d4",
-    "invisible": "#00000000",
-    "match_alpha": "d0",
-}
-
 white = {
-    **light,
+    "theme_type": "light",
     "theme_name": "Logos White",
-    "bg_main": "#ffffff",
-    "bg_subtle": "#f8f8f8",
-    "bg_fade": "#f2f2f2",
-    "bg_hl": "#ebebeb",
-    "bg_strong": "#e3e3e3",
-    "fg_faint": "#c8c8c8",
-    "fg_fade": "#a8a8a8",
-    "fg_dim": "#808080",
-    "fg_subtle": "#666666",
-    "fg_main": "#111111",
-    "accent_fg": "#667bb8",
+    "base_0": "#ffffff",
+    "base_1": "#f8f8f8",
+    "base_2": "#f2f2f2",
+    "base_3": "#e8e8e8",
+    "base_4": "#000000",
+    "base_5": "#000000",
+    "fg_accent": "#6078aa",
+    "bg_accent": "#bbd0e8",
+    "red_1": "#bb5555",
+    "red_2": "#e89090",
+    "orange_1": "#d08868",
+    "orange_2": "#f0ccb0",
+    "yellow_1": "#aa8877",
+    "yellow_2": "#eed8bb",
+    "green_1": "#779955",
+    "green_2": "#b0c088",
+    "blue_1": "#6677aa",
+    "blue_2": "#bbd0e8",
+    "magenta_1": "#aa7788",
+    "magenta_2": "#d8b8c8",
+    "cyan_1": "#77aaaa",
+    "cyan_2": "#b8dbd4",
+    "invisible": "#00000000",
 }
 
 sun = {
     **white,
     "theme_name": "Logos Sun",
-    "bg_main": "#fffffa",
-    "bg_subtle": "#f8f8f3",
-    "bg_fade": "#f2f2ed",
-    "bg_hl": "#ebebe6",
-    "bg_strong": "#e3e3de",
-    "fg_faint": "#c8c8c3",
-    "fg_main": "#000000",
+    "base_0": "#fffffa",
+    "base_1": "#f8f8f3",
+    "base_2": "#f2f2ed",
+    "base_3": "#e8e8e3",
 }
 
 paper = {
-    **light,
+    **white,
     "theme_name": "Logos Paper",
-    "bg_main": "#faf7f1",
-    "bg_subtle": "#f0ebe4",
-    "bg_fade": "#eae5de",
-    "bg_hl": "#e6ded4",
-    "bg_strong": "#d2c2bc",
-    "fg_faint": "#c2b2ac",
-    "fg_fade": "#b2a29c",
-    "fg_dim": "#92827c",
-    "fg_subtle": "#605a52",
-    "fg_main": "#201b12",
-    "fg_bold": "#000000",
-    "accent_fg": "#6670aa",
+    "base_0": "#faf8f0",
+    "base_1": "#f2ede6",
+    "base_2": "#eae5de",
+    "base_3": "#e6ded4",
+    "base_4": "#201b12",
 }
 
 acme = {
-    **light,
+    **white,
     "theme_name": "Logos Acme",
-    "bg_main": "#ffffeb",
-    "bg_subtle": "#f8f8e5",
-    "bg_fade": "#f2f2df",
-    "bg_hl": "#ebebd8",
-    "bg_strong": "#d8d8c5",
-    "fg_faint": "#ccccb7",
-    "fg_fade": "#aaaaa0",
-    "fg_dim": "#888880",
-    "fg_subtle": "#666660",
-    "fg_main": "#000000",
-    "fg_bold": "#000000",
-    "bg_match": "#eeeeaa",
-    "accent_fg": "#777dbb",
+    "base_0": "#ffffea",
+    "base_1": "#f8f8e3",
+    "base_2": "#f2f2dd",
+    "base_3": "#e8e8d3",
+    "red_1": "#bb5d5d",
+    "red_2": "#ee9999",
+    "yellow_1": "#998866",
+    "yellow_2": "#eeeeaa",
+    "green_1": "#448844",
+    "green_2": "#bbddbb",
+    "blue_1": "#5577aa",
+    "blue_2": "#ccddee",
+    "magenta_1": "#aa77aa",
+    "magenta_2": "#ddbbdd",
+    "cyan_1": "#66aaaa",
+    "cyan_2": "#addddd",
+    "fg_accent": "#7777b0",
+    "bg_accent": "#ccccee",
 }
 
 dark = {
-    "theme_type": "dark",
     "theme_name": "Logos Dark",
-    "bg_main": "#282828",
-    "bg_subtle": "#303030",
-    "bg_fade": "#383838",
-    "bg_hl": "#444444",
-    "bg_strong": "#636363",
-    "bg_match": "#77ccaa",
-    "fg_faint": "#666666",
-    "fg_fade": "#888888",
-    "fg_dim": "#a8a8a8",
-    "fg_subtle": "#b8b8b8",
-    "fg_main": "#d8d8d8",
-    "fg_bold": "#eeeeee",
-    "accent_fg": "#88c4a0",
+    "theme_type": "dark",
+    "base_0": "#2b2b2b",
+    "base_1": "#333333",
+    "base_2": "#3b3b3b",
+    "base_3": "#484848",
+    "base_4": "#dddddd",
+    "base_5": "#ffffff",
     "red_1": "#f87870",
     "orange_1": "#ddaa77",
     "yellow_1": "#d0a868",
     "green_1": "#a9b665",
-    "blue_1": "#7daea3",
+    "blue_1": "#80a8dd",
     "magenta_1": "#d3889b",
-    "cyan_1": "#88b490",
+    "cyan_1": "#88b4a0",
+    "fg_accent": "#88d4c0",
+    "bg_accent": "#88d4c0",
     "invisible": "#00000000",
-    "match_alpha": "88",
 }
 
-warm = {
+gruv = {
     **dark,
-    "theme_name": "Logos Warm",
-    "bg_main": "#32302f",
-    "bg_main": "#292828",
-    "bg_subtle": "#3a3837",
-    "bg_fade": "#403d3c",
-    "bg_hl": "#504e4c",
-    "bg_strong": "#5a5958",
-    "fg_faint": "#6c5f54",
-    "fg_fade": "#8c7f74",
-    "fg_dim": "#a89984",
-    "fg_subtle": "#c8b8a4",
-    "fg_main": "#e0d0bb",
+    "theme_name": "Logos Gruv",
+    "base_0": "#32302f",
+    "base_1": "#3a3837",
+    "base_3": "#403d3c",
+    "base_3": "#5c544b",
+    "base_4": "#e0d0bb",
 }
 
 nord = {
     **dark,
     "theme_name": "Logos Nord",
-    "bg_main": "#2e3440",
-    "bg_subtle": "#363d4c",
-    "bg_fade": "#3c4352",
-    "bg_hl": "#434c5e",
-    "bg_strong": "#5c6372",
-    "fg_faint": "#505b70",
-    "fg_fade": "#808ba0",
-    "fg_dim": "#98a5be",
-    "fg_subtle": "#b8c5de",
-    "fg_main": "#d8dee9",
-    "fg_bold": "#eceff4",
-    "accent_fg": "#88c0d0",
-    "bg_match": "#88c0d0",
-    "red_1": "#cf818a",
+    "base_0": "#2e3440",
+    "base_1": "#363d4c",
+    "base_2": "#3c4352",
+    "base_3": "#434c5e",
+    "base_4": "#d8dee9",
+    "base_5": "#e8eef9",
+    "red_1": "#bf616a",
     "orange_1": "#d08770",
     "green_1": "#a3be8c",
     "yellow_1": "#ebcb8b",
     "blue_1": "#81a1c1",
     "magenta_1": "#b48ead",
     "cyan_1": "#88c0d0",
+    "fg_accent": "#88c0d0",
+    "bg_accent": "#88c0d0",
 }
 
 red = {
     **dark,
     "theme_name": "Logos Red",
-    "bg_main": "#110000",
-    "bg_subtle": "#221111",
-    "bg_fade": "#281818",
-    "bg_hl": "#442222",
-    "bg_strong": "#663333",
-    "fg_faint": "#663333",
-    "fg_fade": "#995555",
-    "fg_dim": "#bb6666",
-    "fg_subtle": "#dd7777",
-    "fg_main": "#ee8888",
-    "fg_bold": "#ff9999",
-    "accent_fg": "#ff9999",
-    "bg_match": "#bb6666",
+    "base_0": "#000000",
+    "base_1": "#221111",
+    "base_2": "#281818",
+    "base_3": "#442222",
+    "base_4": "#ee8888",
+    "base_5": "#ff9999",
     "red_1": "#cc6666",
     "green_1": "#cc6666",
     "yellow_1": "#cc6666",
@@ -176,6 +137,8 @@ red = {
     "magenta_1": "#cc6666",
     "cyan_1": "#cc6666",
     "orange_1": "#cc6666",
+    "fg_accent": "#ff9999",
+    "bg_accent": "#995555",
 }
 
 themes = {
@@ -187,24 +150,54 @@ themes = {
     },
     "dark": {
         "dark": dark,
-        "warm": warm,
+        "gruv": gruv,
         "nord": nord,
         "red": red,
     },
 }
 
-for theme_name, theme in themes["light"].items():
-    theme["border_light"] = theme["fg_main"] + "28"
-    theme["border_strong"] = theme["fg_main"] + "60"
-    theme["bg_drop"] = theme["bg_hl"] + "88"
-    theme["black_1"] = theme["fg_main"]
-    theme["white_2"] = theme["bg_subtle"]
+for category in themes.values():
+    for theme_name, theme in category.items():
+        theme["bg_main"] = theme["base_0"]
+        theme["bg_ui"] = theme["base_0"]
+        theme["bg_subtle"] = theme["base_1"]
+        theme["bg_fade"] = theme["base_2"]
+        theme["bg_hl"] = theme["base_3"]
+        theme["bg_drop"] = theme["bg_hl"] + "88"
+        theme["fg_main"] = theme["base_4"]
+        theme["fg_bold"] = theme["base_5"]
+        theme["link"] = theme["fg_accent"]
 
-for theme_name, theme in themes["dark"].items():
-    theme["border_light"] = theme["fg_main"] + "38"
-    theme["border_strong"] = theme["fg_main"] + "50"
-    theme["bg_drop"] = theme["bg_hl"] + "88"
-    theme["black_1"] = theme["bg_main"]
-    theme["white_2"] = theme["fg_main"]
-    for color in ["red", "green", "yellow", "blue", "magenta", "cyan", "orange"]:
-        theme[f"{color}_2"] = theme[f"{color}_1"]
+        if theme in themes["light"].values():
+            theme["fg_faint"] = theme["fg_main"] + "33"
+            theme["fg_fade"] = theme["fg_main"] + "70"
+            theme["fg_dim"] = theme["fg_main"] + "90"
+            theme["fg_subtle"] = theme["fg_main"] + "aa"
+            theme["bg_match_1"] = theme["bg_accent"] + "bb"
+            theme["bg_match_2"] = theme["yellow_2"] + "bb"
+            theme["black_1"] = theme["fg_main"]
+            theme["white_2"] = theme["bg_main"]
+            theme["border_1"] = theme["fg_fade"]
+            theme["border_2"] = theme["fg_faint"]
+
+        elif theme in themes["dark"].values():
+            theme["fg_faint"] = theme["fg_main"] + "68"
+            theme["fg_fade"] = theme["fg_main"] + "80"
+            theme["fg_dim"] = theme["fg_main"] + "a0"
+            theme["fg_subtle"] = theme["fg_main"] + "c0"
+            theme["bg_match_1"] = theme["fg_accent"] + "77"
+            theme["bg_match_2"] = theme["orange_1"] + "77"
+            theme["black_1"] = theme["bg_main"]
+            theme["white_2"] = theme["fg_main"]
+            theme["border_1"] = theme["fg_main"] + "50"
+            theme["border_2"] = theme["fg_main"] + "38"
+            for color in [
+                "red",
+                "green",
+                "yellow",
+                "blue",
+                "magenta",
+                "cyan",
+                "orange",
+            ]:
+                theme[f"{color}_2"] = theme[f"{color}_1"]
