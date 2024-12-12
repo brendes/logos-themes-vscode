f61ae0a0d0f113c7e257a9db29e34f964a90cbbf
Brendan Desmond <brendes@gmail.com>
2024-12-11 23:22:29 -0500

    ui color distinction is better

--- src/themes.py
+++ src/themes.py
@@ -129,6 +129,7 @@ themes = {
 for theme_name, theme in themes.items():
     theme["bg_main"] = theme["base_0"]
     theme["bg_subtle"] = theme["base_1"]
+    theme["bg_ui"] = theme["base_1"]
     theme["bg_fade"] = theme["base_2"]
     theme["bg_hl"] = theme["base_3"]
     theme["bg_sel"] = theme["base_3"] + "ee"
